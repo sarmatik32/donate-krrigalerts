d7be1bb10d1ee9f97702d6d7f7eb9e4d40c46be8
SARMATCorp <80682424+sarmatik32@users.noreply.github.com>
2026-08-11 23:09:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,22 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/donate-krrig-alerts/',
+
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
-      // Якщо вихідний код у папці src:
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
-  host: '0.0.0.0',
-  port: 3000,
-  allowedHosts: true,
-  hmr: {
-    clientPort: 443, // Допомагає Vite HMR працювати через хмарний проксі
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: true,
+    hmr: {
+      clientPort: 443,
+    },
   },
-},
 });
