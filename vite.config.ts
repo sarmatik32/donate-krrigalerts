0e86fd5097cfb6cdcdbeaf1833ee41078769b9c3
SARMATCorp <80682424+sarmatik32@users.noreply.github.com>
2026-08-12 09:33:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/donate-krrigalerts/',
+  base: '/',
 
   plugins: [react(), tailwindcss()],
 
